398bd2072fa32c3a68e5562ab4ab1de651f61161
Melanie S <melanie.sapiandante@gmail.com>
2023-03-23 16:11:30 -0400

    worked on Javascript

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passwordLength = [];
+ 
+    for (var i = 8; i <= 128; i++) {
+       passwordLength.push(i);
+    }
+ 
+    console.log(passwordLength)
+
+window.prompt(passwordLength)
+
+//var characterTypes = ["special characters", "no special characters"] //use confirm method for character type//
+
+//console.log(characterTypes)//
+
+  // Ask user for their choice
+  var userChoice = window.prompt("Enter number between 8 and 128 for password legnth");
+
+  //var userChoice2 = window.prompt2("Does password include special characters?")//
+
+  
+  
+  //if (userChoice.includes(passwordLength)) {
+    //prompt2();
+  // else if (userChoice2.includes(characterTypes)) {
+    //writePassword();
+   //
+
