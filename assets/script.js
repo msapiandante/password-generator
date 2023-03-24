11b9251ff9cd5e0a458dde8b7ceceb3fb9d6db99
Melanie S <melanie.sapiandante@gmail.com>
2023-03-23 23:44:25 -0400

    debugging JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,33 @@
-var passwordLength = []
-for (var i = 8; i <= 128; i++) {
-    passwordLength.push(i)
-}
+//var passwordLength = [] 
+//for (var i = 8; i <= 128; i++) {
+    //passwordLength.push(i)
+//}
 
-console.log(passwordLength)
+//console.log(passwordLength)
+
+//var generatePassword = function() {
+    // Ask user for their choice
+    //if (passwordLength === (var i = 8; i <= 128; i++))
+       //passwordLength.push(i)
+   // }
+       // window.prompt("Please select at least 1 character types for password" lowerCase,upperCase, numeric, specialChar);
 //need arrays defined for different types of character types//
-var upperCase =[]
-var lowerCase = []
-var numeric = []
-var specialChar = [] 
+//var upperCase =[]
+//var lowerCase = []
+//var numeric = []
+//var specialChar = [] 
+    //else if character types selected then // 
+//user must select at least 1 character type to move on// 
 
+//generateBtn.addEventListener("click", writePassword);
+//var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+//function writePassword() {
+ // var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+//}
+
+  //passwordText.value = password;
 
 // I'm thinking using math function somehwere within JS to pick the length of password. where to put it? I don't know. towards the bottom. what else to put with it? not sure yet Math.floor(Math.random() * 128) + 8;
