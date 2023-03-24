06705daf60c6de29eb2f9873de55ad4d1911326c
Melanie S <melanie.sapiandante@gmail.com>
2023-03-24 15:04:04 -0400

    debugged specialChar array

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,3 @@
-//var passwordLength = [] 
-//for (var i = 8; i <= 128; i++) {
-    //passwordLength.push(i)
-//}
-
-//console.log(passwordLength)
-
-//var generatePassword = function() {
-    // Ask user for their choice
-    //if (passwordLength === (var i = 8; i <= 128; i++))
-       //passwordLength.push(i)
-   // }
-       // window.prompt("Please select at least 1 character types for password" lowerCase,upperCase, numeric, specialChar);
-//need arrays defined for different types of character types//
-//var upperCase =[]
-//var lowerCase = []
-//var numeric = []
-//var specialChar = [] 
-    //else if character types selected then // 
-//user must select at least 1 character type to move on// 
-
 //generateBtn.addEventListener("click", writePassword);
 //var generateBtn = document.querySelector("#generate");
 
@@ -31,3 +10,13 @@
   //passwordText.value = password;
 
 // I'm thinking using math function somehwere within JS to pick the length of password. where to put it? I don't know. towards the bottom. what else to put with it? not sure yet Math.floor(Math.random() * 128) + 8;
+
+
+var passwordLength = []
+for (var i = 8; i <= 128; i++) {
+    passwordLength.push(i)
+}
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numeric = ["0","1","2","3","4","5","6","7","8","9"]
+var specialChar = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":","\"","\\", ";", "@", "#","$","%"]
