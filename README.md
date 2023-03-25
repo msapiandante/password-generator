1e43031c4ee2aa91f9a0d95f831884de74ec0d01
Melanie S <melanie.sapiandante@gmail.com>
2023-03-24 22:26:45 -0400

    debugged javascript, html and CSS, also worked on README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # password-generator
+# <Your-Project-Title>
+
+## Description
+
+I was tasked with creating a functioning random password generator for an employer to give their employees. As technology continues to evolve and grow, more and more of people's information is at risk to identity theft. A random password generator would help the employer keep data for their company safer.I learned about arrays, functions, if and else if statements in JavaScript. I also learned how to link JavaScript to the HTML. Unfortunately, I still have much to learn as I could not debug my code prior to assignment submission. 
+
+## Credits
+https://www.w3schools.com/js/js_functions.asp
+
+ttps://stackoverflow.com/questions/17612778/how-to-define-special-characters-in-string-array-java#:~:text=Use%20%22%5C%22%22%20and%20%22%5C%5C%22%20respectively.
+
+## License
+MIT License
+
+Copyright (c) 2023 msapiandante
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Tests
--- assets/script.js
+++ assets/script.js
@@ -4,30 +4,19 @@ var numeric = ["0","1","2","3","4","5","6","7","8","9"]
 var specialChar = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":","\"","\\", ";", "@", "#","$","%"]
 var enter;
 var choices;
-var selectNumeric;
+var selectNumeric; 
 var selectSpecial; 
-var selectUppercase;
-var selectLowercase;
+var selectUppercase; 
+var selectLowercase; 
 var password; 
+var generateBtn;
 
-Function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password; 
-
-}
-
-generateBtn.addEventListener("click", writePassword);
-
-var generateBtn = document.querySelector("#generate"); 
-
-Function generatePassword() {
-Enter = prompt(“Password must be between 8 and 128 characters long. Please enter value between 8-128.”); 
-If (!enter) {
-alert(“Please enter value between 8-128 to continue.”);
+function generatePassword() {
+enter = prompt("Password must be between 8 and 128 characters long. Please enter value between 8-128."); 
+if (!enter) {
+    alert("Please enter value between 8 and 128 to continue.")
 } else if (enter < 8 || enter > 128) {
-Enter = prompt("Please enter value between 8-128 to continue.")
+enter = prompt("Please enter value between 8-128 to continue.") 
 return;
 } else {
 selectNumeric = confirm("Include numeric characters?");
@@ -39,36 +28,74 @@ selectLowercase = confirm("Include lowercase characters?");
 if (!selectNumeric && !selectSpecial && !selectUppercase && !selectLowercase) {
     choices = alert("Please select at least 1 character type to proceed.")
  return``;
+
 } else if (selectNumeric && selectSpecial && selectUppercase && selectLowercase) {
     choices = specialChar.concat(numeric,upperCase,lowerCase);
+
 } else if (selectNumeric && selectSpecial && selectUppercase); {
     choices = specialChar.concat(numeric,upperCase); 
 } else if (selectNumeric && selectSpecial && selectLowercase); {
     choices = specialChar.concat(numeric, lowerCase);
+} else if (selectSpecial && selectUppercase && selectLowercase); {
+    choices = specialChar.concat(upperCase,lowerCase)
+
 } else if (selectNumeric && selectSpecial && selectLowercase); {
     choices = numeric.concat(specialChar, lowerCase);
+
 } else if (selectNumeric && selectSpecial && selectUppercase); {
     choices = numeric.concat(specialChar, upperCase);
+
+} else if (selectNumeric && selectUppercase && selectLowercase); {
+    choices = numeric.concat(lowerCase, upperCase);
+
+} else if (selectUppercase && selectLowercase && specialSpecial); {
+    choices = upperCase.concat(lowerCase,specialChar);
+
+} else if (selectUppercase && selectLowercase && selectNumeric); {
+    choices = upperCase.concat(lowerCase, numeric);
+
+} else if (selectUppercase && selectSpecial && selectNumeric); {
+    choices = upperCase.concat(specialChar,numeric);
+
 } else if (selectLowercase && selectNumeric) {
     choices = lowerCase.concat(numeric);
+
 } else if (selectLowercase && selectUppercase) {
     choices = lowerCase.concat(upperCase);
+
 } else if (selectNumeric && selectUppercase) {
     choices = numeric.concat(upperCase);
+
 } else if (selectSpecial) {
     choices = specialChar;
+
 } else if (selectNumeric) {
     choices = numeric;
+
 } else if (selectLowercase) {
     choices = lowerCase;
+
 } else if (selectUppercase) {
     choices = upperCase;
 } else 
 
 for (var i = 0; i < enter.length; i++) {
-    var pickChoices = choices.[Math.floor(Math.random() *choices.length)]
+    var pickChoices = choices[Math.floor(Math.random() *choices.length)]
     password.push(pickChoices);
  }return;
 }
 
+generateBtn.addEventListener("click", writePassword);
 
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password; 
+
+}
+ 
+generatePassword();
+writePassword();
